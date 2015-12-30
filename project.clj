b68e4deb8b0ec0c79e9a5b90192439ef17996e4c
Howard M. Lewis Ship <hlship@walmartlabs.com>
2015-12-30 09:26:50 -0800

    Use latest version of toolchest

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache Software License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :eval-in-leiningen true
-  :dependencies [[io.aviso/toolchest "0.1.2"]
+  :dependencies [[io.aviso/toolchest "0.1.3"]
                  ;; This version of dependency is compatible with
                  ;; Clojure < 1.7.
                  [com.stuartsierra/dependency "0.1.1"]
